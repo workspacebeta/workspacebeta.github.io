77ba36747987bde0e38fa54dffa2247961964e84
workspacebeta <chinmoyuk@gmail.com>
2016-08-09 00:18:48 +0530

    Upload all
    Bulk upload

---
+++ applications/report-ext-a/app_old.js
@@ -0,0 +1,25 @@
+$("#ws-explore").html('<div class="row"><div class="col-md-12"><h2>Report External A</h2><p>Report (customized) with external activity and marked as A.</p><p>Note: Contains business report, please make sure that the Project databse is not in use!</p><p>Format : Debmalya , Souvik , Sneha , Akanksha , Shaonti , Arka , Sudeshna etc.</p></div></div><div class="row"><div class="col-md-12"><div id="ws-ui-112345"><form method="post" onsubmit="return wsPageForm_112346_01();"><ul class="fieldlist"><li><label for="u_Query">User Name Search</label><input id="u_Query" type="text" class="k-textbox" style="width: 50%;" required /></li><li><label for="u_Month">Month</label><input id="u_Month" style="width: 50%;" required /></li><li id="u_Msg"></li><li><button id="ws-112345-01" type="submit" class="k-button k-primary">Finish</button></li></ul></form><style>.fieldlist {margin: 10px 0; padding: 0;} .fieldlist li {list-style: none;} .fieldlist label {display: block; padding-bottom: 5px; font-weight: bold; text-transform: uppercase; font-size: 12px; color: #444;}</style></div></div></div>');
+
+function wsPageForm_112346_01(){
+	_wsProgress(80);
+	$.getJSON('https://script.google.com/macros/s/AKfycbyBeaAyqxk2iphU72PsWmmHein8fwd17s5a0EKPhXiYVNYpM88/exec?type=report-ext-a&month='+$("#u_Month").val()+'&values='+$("#u_Query").val()).done(function(result){
+		_wsProgress(100);
+		if(result){
+			_msg('Success: Report generated');
+		}else{
+			_msg('Sorry! Unable to process your request');
+		}
+	}).fail(function(result){
+		_msg('Error: Connection problem'); _wsProgress(100);
+	});
+	
+	return false;
+}
+	
+	$("#u_Month").kendoDatePicker({
+		start: "year",
+		depth: "year",
+		format: "yyyy-MM"
+	});
+
+_url('Report External A', '#Report/ext-a');
